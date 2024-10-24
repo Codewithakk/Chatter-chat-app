0a5fbf08a70917b8e88f8c3f71292ff5b3722fb4
Akshay Kumar Singh <140572866+Codewithakk@users.noreply.github.com>
2024-10-24 23:27:11 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -3,31 +3,41 @@ const connectDB = require("./db.js");
 const cors = require("cors");
 const http = require("http");
 const dotenv = require("dotenv");  // Import dotenv
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;  // Use PORT from environment variables or default to 5000
 
 const app = express();
-dotenv.config({ path: "./.env" });  // Use dotenv after importing
 
-app.use(cors());
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(express.json({ limit: "50mb" }));
+// Load environment variables from .env file
+dotenv.config({ path: "./.env" });
+
+// CORS configuration
+const allowedOrigins = ['https://chatters-chat-app.netlify.app']; // Add your allowed origin here
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the methods you want to allow
+  credentials: true // Allow credentials (cookies, authorization headers, etc.)
+}));
+
+// Middleware
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));  // Parse URL-encoded bodies
+app.use(express.json({ limit: "50mb" }));  // Parse JSON bodies
 
 // Routes
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-app.use("/user", require("./Routes/auth_routes.js"));
-app.use("/message", require("./Routes/message_routes.js"));
-app.use("/conversation", require("./Routes/conversation_routes.js"));
+app.use("/user", require("./Routes/auth_routes.js"));  // User authentication routes
+app.use("/message", require("./Routes/message_routes.js"));  // Message handling routes
+app.use("/conversation", require("./Routes/conversation_routes.js"));  // Conversation handling routes
 
 // Server setup
 const server = http.createServer(app);
 
 // Socket.io setup
-require("./socket.js")(server); // Initialize socket.io logic
+require("./socket.js")(server);  // Initialize socket.io logic
 
-// Start server and connect to database
+// Start server and connect to the database
 server.listen(PORT, () => {
   console.log(`🚀 Server started at http://localhost:${PORT}`);
-  connectDB();
+  connectDB();  // Connect to the MongoDB database
 });
